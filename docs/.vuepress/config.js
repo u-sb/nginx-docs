d28911b149c26ba81959196e759426f8b0fe5d19
Showfom <showfom@gmail.com>
2021-07-26 02:44:45 +0800

    Update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -19,19 +19,15 @@ module.exports = {
     },
     head: [
       ['link', { rel: 'icon', href: '/favicon.ico' }],
-// add matomo analytics
-    ['script', {}, `
-    var _paq = window._paq = window._paq || [];
-    _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']);
-    (function() {
-      var u="https://stat.loli.net/";
-      _paq.push(['setTrackerUrl', u+'matomo.php']);
-      _paq.push(['setSiteId', '7']);
-      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-      g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-    })();`
-  ],
+// add Plausible analytics
+['script', {}, `
+const script = document.createElement('script');
+script.async = true;
+script.defer = true;
+script['data-domain'] = 'nginx.io';
+script.src = 'https://stat.xtom.com/js/plausible.js';
+document.head.appendChild(script);`
+],
     ],
     markdown: {
       lineNumbers: false
